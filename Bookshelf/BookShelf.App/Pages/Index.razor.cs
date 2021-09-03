808ef4dc51afda1fec5b141cbbf95f8e0720070e
PiotrWalczak2001 <01.piotrwalczak@gmail.com>
2021-09-03 17:05:27 +0200

    Logout feature add + navigation panel changes
    Authorization view for nav components

---
+++ Bookshelf/BookShelf.App/Pages/Index.razor.cs
@@ -0,0 +1,13 @@
+﻿using BookShelf.App.Auth;
+using BookShelf.App.Contracts;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Threading.Tasks;
+
+namespace BookShelf.App.Pages
+{
+    public partial class Index
+    {
+        
+    }
+}
---
+++ Bookshelf/BookShelf.App/Pages/Logout.razor.cs
@@ -0,0 +1,24 @@
+﻿using BookShelf.App.Contracts;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookShelf.App.Pages
+{
+    public partial class Logout
+    {
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        [Inject]
+        public IAuthenticationService AuthenticationService { get; set; }
+
+        protected async override Task OnInitializedAsync()
+        {
+            await AuthenticationService.Logout();
+            NavigationManager.NavigateTo("/");
+        }
+    }
+}
